d06f84f914c35ede17d7fa695fc8ddb41b7f2e99
hazridahaw-png <hazridahaw@gmail.com>
2025-12-10 07:40:17 +0000

    Added new file data.js

--- data.js
+++ data.js
@@ -1,1 +1,15 @@
-const JSON 
+const JSON_BIN_BASE_URL="https://api.jsonbin.io/v3 ";
+const JSON_BIN_ID="69391b3343b1c97be9e3d2ad";
+const MASTER_KEY="$2a$10$FfkuWvgueaD7i4YlS0UUeeo.u49.PZ5v40knLY33yO7UWgDrDNsqG";
+
+async function loadData() {
+  const config = {
+    "headers": {
+      "Content-Type":"application/json",
+      "X-Master-Key": MASTER_KEY
+
+    }
+  }
+  const response = await axios.get('${JASON_BIN_BASE_URL}/b/{(JSON BIN ID}/latest');
+  console.log(response.data)
+}
